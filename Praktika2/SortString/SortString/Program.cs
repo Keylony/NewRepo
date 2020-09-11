8bcb045875ec05ad2f957c07669f3ba263450179
Keylony <student@C320U23>
2020-09-11 11:44:58 +0300

    create rab 3

---
+++ Praktika2/SortString/SortString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortString
+{
+  class Program
+  {
+    string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+    string[] sa = s.Split(“ “);
+
+    Array.Sort(sa);
+
+      s = string.Join(" ", sa); Console.WriteLine(s);
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
